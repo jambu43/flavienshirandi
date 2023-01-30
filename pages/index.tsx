2b78f037410157fc00d307386b0ff0ef5c58a920
Joel AMBU <ambujoel@gmail.com>
2023-01-30 10:22:40 +0100

    Feat: get in touch section

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Article from "../components/home/Article";
 import Banner from "../components/home/Banner";
 import Brand from "../components/home/Brand";
 import Expertise from "../components/home/Expertise";
+import GetTouch from "../components/home/GetTouch";
 import Layout from "../components/Layout";
 
 const Home: NextPage = () => {
@@ -19,6 +20,7 @@ const Home: NextPage = () => {
         <About />
         <Article />
         <Expertise />
+        <GetTouch />
       </Layout>
     </div>
   );
