e0276137e33a8a39e86f701335de348bc0946446
Joel AMBU <ambujoel@gmail.com>
2023-02-03 02:35:31 +0100

    Feat: lineargradient color

---
+++ components/about/Banner.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import React from "react";
+import styled from "styled-components";
+
+function Banner() {
+  return (
+    <BannerComponent>
+ 
+    </BannerComponent>
+  );
+}
+
+const BannerComponent = styled.div`
+  height: 900px;
+  display: flex;
+  width: 100%;
+  padding: 0 5rem;
+  align-items: center;
+`;
+
+
+export default Banner;
---
+++ pages/about.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Banner from '../components/about/Banner'
+import Layout from '../components/Layout'
+import styled from 'styled-components'
+
+function about() {
+  return (
+    <Layout>
+      <BasedComponent>
+        <Banner />
+      </BasedComponent>
+    </Layout>
+  )
+}
+
+
+const BasedComponent = styled.div`
+  /* background: linear-gradient(142.47deg, rgba(58, 229, 96, 0.31) 0.78%, rgba(217, 217, 217, 0) 60.89%); */
+  background: linear-gradient(142.47deg,rgba(170, 242, 186, 0.97) 0.78%,rgb(255, 255, 255) 60.89%);
+
+`;
+export default about
