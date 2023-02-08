428ba52a2d0e5eed6b320e104ea5dfdd66676ff8
Joel AMBU <ambujoel@gmail.com>
2023-02-08 06:14:53 +0100

    Fix: app name

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,6 +1,1 @@
 /** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://topcongo.live',
-    generateRobotsTxt: true, // (optional)
-    // ...other options
-}
--- utils/api.ts
+++ utils/api.ts
@@ -1,3 +1,2 @@
-export const api: string = "https://admin.topcongo.live/api/";
-export const token: string =
-  "wZ12CTK4qCywaYTTJoTTyGMkkyHVeDO469Z023JFReEdbIY52HAmjqbH6cB4jhaNe4q02nZbEiA1biJK";
+export const api: string = "";
+export const token: string = "";
